65e88ec5886e9a24d0e6ca796e3a152ff93d13ce
buildautomation <buildautomation@notbio.com>
2025-10-24 21:00:02 -0700

    infra

---
+++ .terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "4.85.0"
+  constraints = "~> 4.0"
+  hashes = [
+    "h1:ZVDZuhYSIWhCkSuDkwFeSIJjn0/DcCxak2W/cHW4OQQ=",
+    "zh:17d60a6a6c1741cf1e09ac6731433a30950285eac88236e623ab4cbf23832ca3",
+    "zh:1c70254c016439dbb75cab646b4beace6ceeff117c75d81f2cc27d41c312f752",
+    "zh:35e2aa2cc7ac84ce55e05bb4de7b461b169d3582e56d3262e249ff09d64fe008",
+    "zh:417afb08d7b2744429f6b76806f4134d62b0354acf98e8a6c00de3c24f2bb6ad",
+    "zh:622165d09d21d9a922c86f1fc7177a400507f2a8c4a4513114407ae04da2dd29",
+    "zh:7cdb8e39a8ea0939558d87d2cb6caceded9e21f21003d9e9f9ce648d5db0bc3a",
+    "zh:851e737dc551d6004a860a8907fda65118fc2c7ede9fa828f7be704a2a39e68f",
+    "zh:a331ad289a02a2c4473572a573dc389be0a604cdd9e03dd8dbc10297fb14f14d",
+    "zh:b67fd531251380decd8dd1f849460d60f329f89df3d15f5815849a1dd001f430",
+    "zh:be8785957acca4f97aa3e800b313b57d1fca07788761c8867c9bc701fbe0bdb5",
+    "zh:cb6579a259fe020e1f88217d8f6937b2d5ace15b6406370977a1966eb31b1ca5",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version     = "0.9.1"
+  constraints = "0.9.1"
+  hashes = [
+    "h1:VxyoYYOCaJGDmLz4TruZQTSfQhvwEcMxvcKclWdnpbs=",
+    "zh:00a1476ecf18c735cc08e27bfa835c33f8ac8fa6fa746b01cd3bcbad8ca84f7f",
+    "zh:3007f8fc4a4f8614c43e8ef1d4b0c773a5de1dcac50e701d8abc9fdc8fcb6bf5",
+    "zh:5f79d0730fdec8cb148b277de3f00485eff3e9cf1ff47fb715b1c969e5bbd9d4",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8c8094689a2bed4bb597d24a418bbbf846e15507f08be447d0a5acea67c2265a",
+    "zh:a6d9206e95d5681229429b406bc7a9ba4b2d9b67470bda7df88fa161508ace57",
+    "zh:aa299ec058f23ebe68976c7581017de50da6204883950de228ed9246f309e7f1",
+    "zh:b129f00f45fba1991db0aa954a6ba48d90f64a738629119bfb8e9a844b66e80b",
+    "zh:ef6cecf5f50cda971c1b215847938ced4cb4a30a18095509c068643b14030b00",
+    "zh:f1f46a4f6c65886d2dd27b66d92632232adc64f92145bf8403fe64d5ffa5caea",
+    "zh:f79d6155cda7d559c60d74883a24879a01c4d5f6fd7e8d1e3250f3cd215fb904",
+    "zh:fd59fa73074805c3575f08cd627eef7acda14ab6dac2c135a66e7a38d262201c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,315 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "google" {
+  # Replace with your GCP project ID
+  project = "lagorgeous-helping-hands"
+  # The region for the resources
+  region  = "us-west1"
+}
+
+# Enable necessary APIs
+resource "google_project_service" "gke" {
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "filestore" {
+  service = "file.googleapis.com"
+}
+
+resource "google_project_service" "pubsub" {
+  service = "pubsub.googleapis.com"
+}
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "monitoring" {
+  service = "monitoring.googleapis.com"
+}
+
+resource "time_sleep" "wait_for_api" {
+    create_duration = "60s"
+  depends_on = [google_project_service.compute]
+}
+
+# Pub/Sub Topic for frame processing
+resource "google_pubsub_topic" "frame_processing_topic" {
+  name = "frame-processing-topic"
+  depends_on = [google_project_service.pubsub]
+}
+
+# Pub/Sub Subscription for the hand-finder workers
+resource "google_pubsub_subscription" "hand_finder_subscription" {
+  name  = "hand-finder-subscription"
+  topic = google_pubsub_topic.frame_processing_topic.name
+
+  # 24 hours
+  message_retention_duration = "86400s"
+  # 20 seconds, allows for processing time and retries
+  ack_deadline_seconds = 20
+
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+  enable_message_ordering = false
+  depends_on = [google_pubsub_topic.frame_processing_topic]
+}
+
+# VPC Network
+resource "google_compute_network" "vpc" {
+  name                    = "gke-network"
+  auto_create_subnetworks = false
+  depends_on = [time_sleep.wait_for_api]
+}
+
+# GKE Subnet
+resource "google_compute_subnetwork" "gke_subnet" {
+  name          = "gke-subnet"
+  ip_cidr_range = "10.10.0.0/24"
+  network       = google_compute_network.vpc.self_link
+  region        = "us-west1"
+}
+
+# Enable the Service Networking API
+resource "google_project_service" "servicenetworking" {
+  service = "servicenetworking.googleapis.com"
+  depends_on = [google_project_service.compute]
+}
+
+# Allocate an IP range for the service networking connection
+# Note: You may need to adjust the prefix_length depending on your network requirements.
+resource "google_compute_global_address" "vertex_ai_peering_range" {
+  name          = "vertex-ai-peering-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.id
+  depends_on    = [google_project_service.servicenetworking]
+}
+
+# Create the VPC peering connection
+resource "google_service_networking_connection" "vertex_ai_peering" {
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.vertex_ai_peering_range.name]
+  depends_on              = [google_compute_global_address.vertex_ai_peering_range]
+}
+
+# GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = "gke-cluster"
+  location = "us-west1"
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.gke_subnet.name
+
+  initial_node_count = 1
+
+  node_config {
+    disk_type = "pd-standard"
+  }
+
+  # Enable Filestore CSI driver
+  addons_config {
+    gcp_filestore_csi_driver_config {
+      enabled = true
+    }
+  }
+
+  # We need to create a node pool to run workloads
+  remove_default_node_pool = true
+
+  depends_on = [google_project_service.gke]
+}
+
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-pool"
+  location   = "us-west1"
+  node_locations = ["us-west1-a"]
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+  version = "1.33.5-gke.1080000"
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      "node-pool-type" = "default-pool"
+      "terraform-update-trigger" = "1"
+    }
+  }
+}
+
+resource "google_container_node_pool" "gpu_pool" {
+  name       = "gpu-pool"
+  location   = "us-west1"
+  node_locations = ["us-west1-a"]
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  lifecycle {
+    ignore_changes = all
+  }
+
+  node_config {
+    machine_type = "g2-standard-4"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      "node-pool-type" = "gpu-pool"
+    }
+
+    guest_accelerator {
+      type  = "nvidia-l4"
+      count = 1
+    }
+  }
+}
+
+resource "google_container_node_pool" "streaming_pool" {
+  name       = "streaming-pool"
+  location   = "us-west1"
+  node_locations = ["us-west1-a"]
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-standard-8"
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    labels = {
+      "node-pool-type" = "streaming-pool"
+    }
+  }
+}
+
+# Filestore Instance
+resource "google_filestore_instance" "nfs_store" {
+  name     = "nfs-store"
+  location = "us-west1-a" # Must be a zone
+  tier     = "STANDARD"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "fileshare"
+  }
+
+  networks {
+    network = google_compute_network.vpc.name
+    modes   = ["MODE_IPV4"]
+  }
+
+  depends_on = [google_project_service.filestore]
+}
+
+# IAM Service Account for accessing the Filestore
+resource "google_service_account" "filestore_accessor" {
+  account_id   = "filestore-accessor"
+  display_name = "Filestore Accessor"
+}
+
+# Grant the Filestore Editor role to the service account
+resource "google_project_iam_member" "filestore_iam" {
+  project = "lagorgeous-helping-hands"
+  role    = "roles/file.editor"
+  member  = "serviceAccount:${google_service_account.filestore_accessor.email}"
+}
+
+# Outputs
+output "gke_cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "gke_cluster_region" {
+  value = google_container_cluster.primary.location
+}
+
+output "filestore_ip_address" {
+  value = google_filestore_instance.nfs_store.networks[0].ip_addresses[0]
+}
+
+output "filestore_file_share_name" {
+  value = google_filestore_instance.nfs_store.file_shares[0].name
+}
+
+resource "google_project_iam_member" "lagorgeous_owner" {
+  project = "lagorgeous-helping-hands"
+  role    = "roles/owner"
+  member  = "user:lagorgeous.creator@gmail.com"
+}
+
+resource "google_project_iam_member" "lagorgeous_cloudbuild_editor" {
+  project = "lagorgeous-helping-hands"
+  role    = "roles/cloudbuild.builds.editor"
+  member  = "user:lagorgeous.creator@gmail.com"
+}
+
+
+
+resource "google_project_iam_member" "perberto_cloudbuild_editor" {
+  project = "lagorgeous-helping-hands"
+  role    = "roles/cloudbuild.builds.editor"
+  member  = "user:perbhatk@gmail.com"
+}
+
+resource "google_monitoring_dashboard" "pubsub_dashboard" {
+  project = "lagorgeous-helping-hands"
+  dashboard_json = <<EOF
+{
+  "displayName": "Pub/Sub Frame Processing",
+  "gridLayout": {
+    "columns": "2",
+    "widgets": [
+      {
+        "title": "Publish Rate",
+        "xyChart": {
+          "dataSets": [
+            {
+              "timeSeriesQuery": {
+                "timeSeriesFilter": {
+                  "filter": "metric.type=\"pubsub.googleapis.com/topic/send_request_count\" resource.type=\"pubsub_topic\" resource.label.\"topic_id\"=\"frame-processing-topic\"",
+                  "aggregation": {
+                    "perSeriesAligner": "ALIGN_RATE",
+                    "crossSeriesReducer": "REDUCE_SUM",
+                    "groupByFields": []
+                  }
+                },
+                "unitOverride": "1/s"
+              },
+              "plotType": "LINE"
+            }
+          ],
+          "timeshiftDuration": "0s",
+          "yAxis": {
+            "label": "Messages per second",
+            "scale": "LINEAR"
+          }
+        }
+      }
+    ]
+  }
+}
+EOF
+}
